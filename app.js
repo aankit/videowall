8a40003c380863751acb4bd0375f7358f9410c23
aankit <Aankit@172-29-131-135.dynapool.nyu.edu>
2014-10-03 14:26:58 -0400

    multi screen capabilities with scaling

--- app.js
+++ app.js
@@ -8,13 +8,31 @@ var ws = require('ws'); // ws is the WebSocket libary
 var app = express();
 var HTTP_port = process.env.PORT || 8000; // the port our server will run on
 
+
 //build screen manager
-var screensConnected = [];
+var screens = {};
+var screenNum;
+var vidFiles = ['Sequence01.mov', 'Sequence02.mov', 'Sequence03.mov', 'Sequence04.mov'];
+var modes = ['all', 'single'];
 
 // we are only using one route, which simply returns the file the browser asks for
-app.get('*', function(req, res){
-	screensConnected[req.query.q]=true;
-	res.sendfile(__dirname + req.url);
+app.use('/current', function(req, res, next){
+	screenNum = req.query.screenNum;
+	next();
+});
+
+app.route('/old')
+	.get(function(req, res){
+		res.sendfile('index1.html');
+});
+
+app.route('/current')
+	.get(function (req, res){
+		res.sendfile('index2.html');
+	});
+
+app.route('*').get(function(req, res){
+	res.sendFile(__dirname + req.url);
 });
 
 // now actually start the server
@@ -23,9 +41,6 @@ var server = http.createServer(app);
 server.listen(HTTP_port);
 
 console.log('HTTP server started on port '+HTTP_port);
-screensConnected.forEach(function(entry){
-	console.log(entry);
-});
 
 ////////////////////////////
 ////////////////////////////
@@ -38,22 +53,24 @@ var socketServer = new WebSocketServer({'server':server}); //create an instance 
 var allSockets = [];
 
 socketServer.on('connection',function(socket){
+	//console.log('socket on');
+	//socketHandlers['videoURL'](socket);
+	//console.log(screens);
 	socket.on('message',function(data){
 		var msg = JSON.parse(data);
 		if(socketHandlers[msg.type]) socketHandlers[msg.type](socket,msg);
 	});
 
 	socket.on('close',function(){
+		console.log('bye');
 		for(var i=0;i<allSockets.length;i++){
 			if(allSockets[i]===socket){
 				allSockets.splice(i,1);
-				//maybe make sure database connection is saved and closed.
-				break;
 			}
 		}
 	});
-
 	allSockets.push(socket);
+	screens.socket = screenNum;
 });
 
 ////////////////////////////
@@ -61,6 +78,25 @@ socketServer.on('connection',function(socket){
 ////////////////////////////
 
 var socketHandlers = {
+	'update': function(socket, msg){
+		// console.log(msg.duration);
+		// console.log(msg.current);
+	},
+	'first': function(socket, msg){
+		for(var i=0;i<allSockets.length;i++){
+			if(allSockets[i]===socket){
+				//look up what I should be playing on this screen
+				var scr = screens.socket;
+				console.log(screens.socket);
+				var vidData = {
+					'videoSource': vidFiles[scr],
+					'mode': 'all',
+					'screenNum': scr
+				};
+				allSockets[i].send(JSON.stringify(vidData));
+			}
+		}
+	},
 	'passAlong':function(socket,msg){
 		for(var i=0;i<allSockets.length;i++){
 			if(allSockets[i]===socket){
